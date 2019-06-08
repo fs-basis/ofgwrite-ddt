e79d617c9cd3495eff95ed28eb53ffef762e5152
BPanther <bpanther_ts@hotmail.com>
2019-06-08 21:40:02 +0200

    progress bar fix (thx DBoxOldie)

--- busybox/libarchive/decompress_bunzip2.c
+++ busybox/libarchive/decompress_bunzip2.c
@@ -819,6 +819,9 @@ unpack_bz2_stream(transformer_state_t *xstate)
 	dealloc_bunzip(bd);
 	free(outbuf);
 
+	// changed for ofgwrite
+	set_step_progress(100);
+
 	return i ? i : IF_DESKTOP(total_written) + 0;
 }
 
