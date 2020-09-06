ff45fe36fd9cd8853816b11f232d2cbc0f9cf396
Frankenstone <dampf_acc@online.de>
2020-09-06 11:17:30 +0200

    Revert "fix for gcc10"
    This reverts commit bdd8966c748e77b5e3b970594b201c3d6da1d0bb.

--- ofgwrite.h
+++ ofgwrite.h
@@ -1,11 +1,11 @@
 #include <sys/stat.h>
 
-static struct stat kernel_file_stat;
-static struct stat rootfs_file_stat;
+struct stat kernel_file_stat;
+struct stat rootfs_file_stat;
 
-static int multiboot_partition;
-static char current_rootfs_device[1000];
-static char vumodel[63];
+int multiboot_partition;
+char current_rootfs_device[1000];
+char vumodel[63];
 
 enum RootfsTypeEnum
 {
